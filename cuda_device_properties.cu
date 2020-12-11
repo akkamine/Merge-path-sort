ca96d59cb802f326bcdbf6423d8f100571184f46
airaime <romeomolina@gmx.fr>
2020-12-11 10:56:22 +0100

    minor fix

--- cuda_device_properties.cu
+++ cuda_device_properties.cu
@@ -2,12 +2,34 @@
 
 int main(void){
 	// on déclare la structure qui servira de conteneur pour les informations qu'on requêtera
-	cudaDeviceProp prop;
+	cudaDeviceProp devProp;
 	// on fait notre requête au premier GPU (indice=0) pour avoir ses informations
-	cudaGetDeviceProperties(&prop, 0);
+	cudaGetDeviceProperties(&devProp, 0);
 	// on affiche ce qui nous intéresse dans ce qui a été récupéré
+    /*
 	printf("maxGridSize = (%d, %d, %d)\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
 	printf("maxThreadsDim = (%d, %d, %d)\n", prop.maxThreadsDim[0], prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
 	printf("maxThreadsPerBlock = %d\n", prop.maxThreadsPerBlock);
 	// voir le type de structure "cudaDeviceProp" dans la doc pour d'autres champs...
+    */
+    
+    printf("Major revision number:         %d\n",  devProp.major);
+    printf("Minor revision number:         %d\n",  devProp.minor);
+    printf("Name:                          %s\n",  devProp.name);
+    printf("Total global memory:           %lu\n",  devProp.totalGlobalMem);
+    printf("Total shared memory per block: %lu\n",  devProp.sharedMemPerBlock);
+    printf("Total registers per block:     %d\n",  devProp.regsPerBlock);
+    printf("Warp size:                     %d\n",  devProp.warpSize);
+    printf("Maximum memory pitch:          %lu\n",  devProp.memPitch);
+    printf("Maximum threads per block:     %d\n",  devProp.maxThreadsPerBlock);
+    for (int i = 0; i < 3; ++i)
+        printf("Maximum dimension %d of block:  %d\n", i, devProp.maxThreadsDim[i]);
+    for (int i = 0; i < 3; ++i)
+        printf("Maximum dimension %d of grid:   %d\n", i, devProp.maxGridSize[i]);
+    printf("Clock rate:                    %d\n",  devProp.clockRate);
+    printf("Total constant memory:         %lu\n",  devProp.totalConstMem);
+    printf("Texture alignment:             %lu\n",  devProp.textureAlignment);
+    printf("Concurrent copy and execution: %s\n",  (devProp.deviceOverlap ? "Yes" : "No"));
+    printf("Number of multiprocessors:     %d\n",  devProp.multiProcessorCount);
+    printf("Kernel execution timeout:      %s\n",  (devProp.kernelExecTimeoutEnabled ?"Yes" : "No"));
 }
